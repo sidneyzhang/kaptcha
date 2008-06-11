c1a450b7fea8f40bd55e4d6cb5ff6abeab8cd3fe
latchkey <latchkey@8ad2362a-652d-0410-8db7-cd0a9423f154>
2008-06-11 03:36:14 +0000

    replace imaging jar with pixels jar
    fixes issue #16
    
    http://code.google.com/p/kaptcha/issues/detail?id=16
    
    git-svn-id: http://kaptcha.googlecode.com/svn/trunk@61 8ad2362a-652d-0410-8db7-cd0a9423f154

--- src/java/com/google/code/kaptcha/impl/ShadowGimpy.java
+++ src/java/com/google/code/kaptcha/impl/ShadowGimpy.java
@@ -1,10 +1,7 @@
 package com.google.code.kaptcha.impl;
 
 import java.awt.Graphics2D;
-import java.awt.Image;
-import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
-import java.awt.image.FilteredImageSource;
 import java.util.Random;
 
 import com.google.code.kaptcha.GimpyEngine;
@@ -35,27 +32,21 @@ public class ShadowGimpy extends Configurable implements GimpyEngine
 
 		ShadowFilter shadowFilter = new ShadowFilter();
 		shadowFilter.setRadius(10);
+		shadowFilter.setDistance(5);
+		shadowFilter.setOpacity(1);
 
 		Random rand = new Random();
 
 		RippleFilter rippleFilter = new RippleFilter();
-		rippleFilter.setWaveType(RippleFilter.SINGLEFRAME);
+		rippleFilter.setWaveType(RippleFilter.SINE);
 		rippleFilter.setXAmplitude(7.6f);
 		rippleFilter.setYAmplitude(rand.nextFloat() + 1.0f);
 		rippleFilter.setXWavelength(rand.nextInt(7) + 8);
 		rippleFilter.setYWavelength(rand.nextInt(3) + 2);
-		rippleFilter.setEdgeAction(TransformFilter.RANDOMPIXELORDER);
+		rippleFilter.setEdgeAction(TransformFilter.BILINEAR);
 
-		rippleFilter.setEdgeAction(TransformFilter.RANDOMPIXELORDER);
-
-		FilteredImageSource rippleFilteredImageSource = new FilteredImageSource(
-				baseImage.getSource(), rippleFilter);
-		Image effectImage = Toolkit.getDefaultToolkit().createImage(
-				rippleFilteredImageSource);
-		FilteredImageSource shadowFilteredImageSource = new FilteredImageSource(
-				effectImage.getSource(), shadowFilter);
-		effectImage = Toolkit.getDefaultToolkit().createImage(
-				shadowFilteredImageSource);
+		BufferedImage effectImage = rippleFilter.filter(baseImage, null);
+		effectImage = shadowFilter.filter(effectImage, null);
 
 		graph.drawImage(effectImage, 0, 0, null, null);
 		graph.dispose();
--- src/java/com/google/code/kaptcha/impl/WaterRipple.java
+++ src/java/com/google/code/kaptcha/impl/WaterRipple.java
@@ -1,10 +1,7 @@
 package com.google.code.kaptcha.impl;
 
 import java.awt.Graphics2D;
-import java.awt.Image;
-import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
-import java.awt.image.FilteredImageSource;
 
 import com.google.code.kaptcha.GimpyEngine;
 import com.google.code.kaptcha.NoiseProducer;
@@ -20,7 +17,7 @@ public class WaterRipple extends Configurable implements GimpyEngine
 {
 	/**
 	 * Applies distortion by adding water ripple effect.
-	 * 
+	 *
 	 * @param baseImage the base image
 	 * @return the distorted image
 	 */
@@ -33,28 +30,21 @@ public class WaterRipple extends Configurable implements GimpyEngine
 		Graphics2D graphics = (Graphics2D) distortedImage.getGraphics();
 
 		RippleFilter rippleFilter = new RippleFilter();
-		rippleFilter.setWaveType(RippleFilter.SINGLEFRAME);
+		rippleFilter.setWaveType(RippleFilter.SINE);
 		rippleFilter.setXAmplitude(2.6f);
 		rippleFilter.setYAmplitude(1.7f);
 		rippleFilter.setXWavelength(15);
 		rippleFilter.setYWavelength(5);
-		rippleFilter.setEdgeAction(TransformFilter.RANDOMPIXELORDER);
+		rippleFilter.setEdgeAction(TransformFilter.NEAREST_NEIGHBOUR);
 
 		WaterFilter waterFilter = new WaterFilter();
-		waterFilter.setAmplitude(4);
-		waterFilter.setAntialias(true);
-		waterFilter.setPhase(15);
-		waterFilter.setWavelength(70);
+		waterFilter.setAmplitude(1.5f);
+		waterFilter.setPhase(10);
+		waterFilter.setWavelength(2);
 
-		FilteredImageSource filteredImageSource = new FilteredImageSource(
-				baseImage.getSource(), waterFilter);
-		Image effectImage = Toolkit.getDefaultToolkit().createImage(
-				filteredImageSource);
+		BufferedImage effectImage = waterFilter.filter(baseImage, null);
+		effectImage = rippleFilter.filter(effectImage, null);
 
-		filteredImageSource = new FilteredImageSource(effectImage.getSource(),
-				rippleFilter);
-		effectImage = Toolkit.getDefaultToolkit().createImage(
-				filteredImageSource);
 		graphics.drawImage(effectImage, 0, 0, null, null);
 
 		graphics.dispose();
